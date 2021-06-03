12a18a91d5a10d78c6c4aaa65679ec6711c41311
ilya <ilya-681@ya.ru>
2021-06-03 21:23:45 +0300

    Added AttachmentsHelper and WebConfig

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -2,22 +2,52 @@ package test;
 
 
 import com.codeborne.selenide.Configuration;
+import config.WebConfig;
 import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static filters.CustomLogFilter.customLogFilter;
+import static helpers.AttachmentsHelper.*;
 
 
 public class TestBase {
     @BeforeAll
     static void setup() {
-
-        RestAssured.filters(customLogFilter().withCustomTemplates());
-        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+        final WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
         Configuration.startMaximized = true;
-        Configuration.baseUrl = "http://demowebshop.tricentis.com";
+        browser = config.getWebDriverBrowser();
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+
+        if (System.getProperty("remote_driver") != null || System.getProperty("env") != null) {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            capabilities.setVersion(config.getWebDriverBrowserVersion());
+            Configuration.browserCapabilities = capabilities;
+
+            Configuration.remote = config.getWebDriverURL();
+
+
+            RestAssured.filters(customLogFilter().withCustomTemplates());
+            RestAssured.baseURI = "http://demowebshop.tricentis.com";
+            Configuration.baseUrl = "http://demowebshop.tricentis.com";
+        }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if (System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
     }
 }
